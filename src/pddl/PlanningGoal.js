59bf87357f47e9eae5f23dc619542f484e892ad7
Marco Robol <marco89mr@gmail.com>
2022-05-20 21:19:40 +0200

    scenario4

--- src/pddl/PlanningGoal.js
+++ src/pddl/PlanningGoal.js
@@ -7,9 +7,9 @@ class PddlGoal extends Goal {
     // Example LightTurnedOn:
     // new LightTurnedOn({goal = [ ['switched-on l1'], ['not switched-off l1'] ]})
     
-    toString() {
-        return this.constructor.name + '#'+this.id + ' goal:' + this.parameters.goal //+ this.effect.map(e=>'('+e+')').join('')
-    }
+    // toString() {
+    //     return this.constructor.name + '#'+this.id + ' goal:' + this.parameters.goal //+ this.effect.map(e=>'('+e+')').join('')
+    // }
 
 }
 
